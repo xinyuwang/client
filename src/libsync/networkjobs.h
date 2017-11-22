4c91f6fba7f9f14d6dbe96342f61179d169ca5f5
Roeland Jago Douma <roeland@famdouma.nl>
2017-11-22 23:00:57 +0100

    Show webview with page
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- src/libsync/networkjobs.h
+++ src/libsync/networkjobs.h
@@ -371,7 +371,8 @@ public:
     enum AuthType {
         Basic, // also the catch-all fallback for backwards compatibility reasons
         OAuth,
-        Shibboleth
+        Shibboleth,
+        WebViewFlow
     };
 
     explicit DetermineAuthTypeJob(AccountPtr account, QObject *parent = 0);
